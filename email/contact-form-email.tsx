e9a3f79d15d6d5fa4a5482f543b6f9f524860453
Hassanjadi <hassanjadi25@gmail.com>
2024-05-11 09:11:24 +0700

    feat: contact form email

--- email/contact-form-email.tsx
+++ email/contact-form-email.tsx
@@ -5,7 +5,6 @@ import {
   Head,
   Heading,
   Hr,
-  Link,
   Container,
   Preview,
   Section,
@@ -13,10 +12,33 @@ import {
 } from "@react-email/components";
 import { Tailwind } from "@react-email/tailwind";
 
-type ContactFromEmailProps = {
+type ContactFormEmailProps = {
   message: string;
+  senderEmail: string;
 };
 
-export const ContactFromEmail = ({ message }: ContactFromEmailProps) => {
-  return <div>ContactFromEmailProps</div>;
+export const ContactFormEmail = ({
+  message,
+  senderEmail,
+}: ContactFormEmailProps) => {
+  return (
+    <Html>
+      <Head />
+      <Preview>New message from your portfolio site</Preview>
+      <Tailwind>
+        <Body className="bg-gray-100 text-black">
+          <Container>
+            <Section className="bg-white borderBlack my-10 px-10 py-4 rounded-md">
+              <Heading className="leading-tight">
+                You received the following message from the contact form
+              </Heading>
+              <Text>{message}</Text>
+              <Hr />
+              <Text>The sender's email is: {senderEmail}</Text>
+            </Section>
+          </Container>
+        </Body>
+      </Tailwind>
+    </Html>
+  );
 };
